1a25aff727488dbeaeefb10dd147c922ac5dfe33
tahir826 <mtahirhassan@gmail.com>
2025-03-02 19:26:20 +0500

    .

--- app/main.py
+++ app/main.py
@@ -30,3 +30,7 @@ app.include_router(user.router, prefix="/users")
 app.include_router(booking.router, prefix="/bookings")
 app.include_router(contact_us.router, prefix="/contact-us")
 app.include_router(admin.router, prefix="/admin")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
